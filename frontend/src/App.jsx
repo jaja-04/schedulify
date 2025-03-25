a868ce0dd1f4492a18f1e3fcb6af6e3e19abf26e
jaja-04 <janine.aguisanda@gmail.com>
2025-03-25 19:12:55 +0800

    Faculty_registration_login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Student from "./components/Student";
+import Faculty from "./components/Faculty";
+import Student_login from "./components/Student_login";
+import Faculty_login from "./components/Faculty_login";
 
 const App = () => {
   return (
@@ -9,6 +12,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="student-register" element={<Student />} />
+        <Route path="student-login" element={<Student_login />} />
+        <Route path="Faculty-register" element={<Faculty />} />
+        <Route path="faculty-login" element={<Faculty_login />} />
       </Routes>
     </Router>
   );
