04a1726c6029c514d07f2673dfdf88e9cce1ad9d
jimm-8 <peacti10@gmail.com>
2025-04-28 12:58:23 +0800

    pre-defined emails2

---
+++ server/models/Student.js
@@ -0,0 +1,51 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../db/db.js';
+import User from './User.js'; // Import User model
+
+const Student = sequelize.define('Student', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  section: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isIn: [['3201', '3202', '3203', '3204', '3205']]
+    }
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'id'
+    }
+  },
+  yearLevel: {
+    type: DataTypes.STRING,
+    allowNull: false
+},
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  tableName: 'student',
+  timestamps: true
+});
+
+// Define the association
+Student.belongsTo(User, { foreignKey: 'userId' });
+User.hasOne(Student, { foreignKey: 'userId' });
+
+export default Student;
